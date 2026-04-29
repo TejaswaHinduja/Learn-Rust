772a69dbeabedcb4227bc2b7f28ae368050b518d
Tejaswa <tejaswahinduja@gmail.com>
2026-04-29 23:19:55 +0530

    learn rust hello world

---
+++ hello.rs
@@ -0,0 +1,11 @@
+fn main(){
+
+    println!("Hello Rust")
+}
+/* why is the print statement like that ?
+Bcz this is a macro and not a standard function , the ! is a indicator which shows that we are calling a macro instead of a normal function
+Normal functions only handle a fixed number of arguments with specific types
+but for the print thing we may have to print N number of things , so macros can handle variable number of arguments
+
+
+*/
