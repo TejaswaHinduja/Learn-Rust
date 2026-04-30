c2cffe3eb47841d8b4a57912627037b56dfd7cc3
Tejaswa <tejaswahinduja@gmail.com>
2026-04-30 23:33:29 +0530

    variable

--- hello.rs
+++ hello.rs
@@ -7,5 +7,12 @@ Bcz this is a macro and not a standard function , the ! is a indicator which sho
 Normal functions only handle a fixed number of arguments with specific types
 but for the print thing we may have to print N number of things , so macros can handle variable number of arguments
 
-
 */
+
+/* Binding Values to names */
+fn main() {
+    let x = 42;
+    let name = "Rust";
+    println!("x = {}", x);
+    println!("name = {}", name);
+}
